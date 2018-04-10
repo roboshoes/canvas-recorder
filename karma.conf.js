cfdb8a5900efd92a7fd5e92fb58850b5c942a88b
Mathias Paumgarten <mail@mathias-paumgarten.com>
2018-04-10 00:26:18 -0400

    adds karma tests

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function( config ) {
+    config.set( {
+        basePath: "",
+        frameworks: [ "mocha", "expect" ],
+        files: [
+            "dist/tests/bundle.js"
+        ],
+        exclude: [],
+        preprocessors: {},
+        reporters: [ "mocha" ],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: [ "Chrome" ],
+        singleRun: true,
+        concurrency: Infinity
+    } );
+};
