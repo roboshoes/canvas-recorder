4048cd88e0fc8dd6eaeb4e54e13c17b6d9c1bb8a
Mathias Paumgarten <mail@mathias-paumgarten.com>
2018-06-15 22:17:08 -0700

    adding gif support for ffmpy

--- bin/index.js
+++ bin/index.js
@@ -9,6 +9,7 @@ program
     .option( "-i, --input <dir>", "Directory containing the image sequence." )
     .option( "-o, --output <name>", "Name of the output file. Defaults to out.mp4." )
     .option( "-r, --fps <num>", "The framerate at which the sequence should play. Default is 30." )
+    .option( "-k, --format <format>", "The file format generated: mp4 or gif. Default is mp4" )
     .parse( process.argv );
 
 
@@ -22,20 +23,43 @@ exec( "ffmpeg -version", function( error, stdout, stderr ) {
         return;
     }
 
+    const format = program.format || "mp4";
     const fps = program.fps || 30;
-    const output = program.output || "out.mp4";
+    const output = program.output || ( "out." + format );
     const input = program.input || ".";
 
-    spawn( "ffmpeg", [
-        "-i %06d.png",
-        `-framerate ${ fps }`,
-        `-r ${ fps }`,
-        "-c:v libx264",
-        `-vf "fps=${ fps },format=yuv420p"`,
-        output,
-    ], {
-        stdio: "inherit",
-        shell: true,
-        cwd: input,
-    } );
+    if ( [ "mp4", "gif" ].indexOf( format ) < 0 ) {
+        console.warn( "Unrecognized format. Supported formats are 'gif' and 'mp4'" );
+        return;
+    }
+
+    switch ( format ) {
+        case "mp4":
+            spawn( "ffmpeg", [
+                "-i %06d.png",
+                `-framerate ${ fps }`,
+                `-r ${ fps }`,
+                "-c:v libx264",
+                `-vf "fps=${ fps },format=yuv420p"`,
+                output,
+            ], {
+                stdio: "inherit",
+                shell: true,
+                cwd: input,
+            } );
+            break;
+        case "gif":
+            spawn( "ffmpeg", [
+                "-f image2",
+                `-framerate ${ fps }`,
+                "-i %06d.png",
+                output,
+            ], {
+                stdio: "inherit",
+                shell: true,
+                cwd: input,
+            } );
+            break;
+    }
+
 } );
