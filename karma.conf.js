a69b43315f3bb28e4353e2e42f2c9812dcbe7f6f
Mathias Paumgarten <mail@mathias-paumgarten.com>
2018-07-10 20:24:25 -0700

    use chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function( config ) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: [ "PhantomJS" ],
+        browsers: [ "ChromeHeadless" ],
         singleRun: true,
         concurrency: Infinity
     } );
