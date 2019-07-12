756825586a16ce460e640edc4de0c7347968e585
Mathias Paumgarten <mail@mathias-paumgarten.com>
2019-07-12 10:42:28 -0700

    cleanup

--- bin/index.js
+++ bin/index.js
@@ -12,6 +12,17 @@ program
     .option( "-k, --format <format>", "The file format generated: mp4 or gif. Default is mp4" )
     .parse( process.argv );
 
+
+/**
+ * Returns the value of a given flag, and falls back to the a given default if flag is not
+ * specified. An optional array of white listed values can restrict the value
+ *
+ * @param {string} name            Value of the flag
+ * @param {string} defaultValue    Default value to be returned if flag not found
+ * @param {string[]=} restrictions Optional. Array of allowed values
+ *
+ * @throws If restrictions are given and the value is not listed, the method throws and error
+ */
 function valueOrDefault( name, defaultValue, restrictions ) {
     const value = program[ name ];
 
@@ -20,12 +31,22 @@ function valueOrDefault( name, defaultValue, restrictions ) {
     }
 
     if ( restrictions && restrictions.indexOf( value ) < 0 ) {
-        throw new Error( name + ' has to be one of: ' + restrictions.join ( ', ' ) );
+        throw new Error( name + " has to be one of: " + restrictions.join ( ", " ) );
     }
 
     return value;
 }
 
+/**
+ * Creates FFMPEG command based on input options.
+ *
+ * @param {Object} options Settings to determine the
+ * @param {string} options.format Output format either "mp4" or "gif";
+ * @param {number} options.fps The frames per second used for both input as well as output
+ * @param {number} options.output Name of the file used as output.
+ *
+ * @returns {string[]} Output command as an array of strings.
+ */
 function getCommand( options ) {
     switch ( options.format ) {
         case "mp4":
@@ -49,7 +70,7 @@ function getCommand( options ) {
     }
 }
 
-exec( "ffmpeg -version", function( error, stdout, stderr ) {
+exec( "ffmpeg -version", function( error, _, stderr ) {
     if ( error || stderr ) {
         console.warn(
             "Error: canvas-recorder requires ffmpeg to be installed and accessable view `ffmpeg` command. " +
@@ -62,14 +83,14 @@ exec( "ffmpeg -version", function( error, stdout, stderr ) {
     const options = {};
 
     try {
+
         options.format = valueOrDefault( "format", "mp4", [ "mp4", "gif" ] );
         options.fps = valueOrDefault( "fps", 30 );
         options.output = valueOrDefault( "output", "out." + options.format );
         options.input = valueOrDefault( "input", "." );
 
     } catch( error ) {
-        console.error( error.message );
-        return;
+        return console.error( error.message );
     }
 
     const spawnOptions = {
